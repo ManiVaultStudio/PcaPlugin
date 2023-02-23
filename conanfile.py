c119b2508a66d72154a1c57553208c5fafbc583d
alxvth <a.vieth@tudelft.nl>
2023-02-23 15:06:53 +0100

    Update conanfile to fix CI build for mac

--- conanfile.py
+++ conanfile.py
@@ -72,6 +72,8 @@ class PcaPluginConan(ConanFile):
         if os_info.is_macos:
             installer = SystemPackageTool()
             installer.install("libomp")
+            proc = subprocess.run("brew --prefix libomp",  shell=True, capture_output=True)
+            subprocess.run(f"ln {proc.stdout.decode('UTF-8').strip()}/lib/libomp.dylib /usr/local/lib/libomp.dylib", shell=True)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -92,7 +94,11 @@ class PcaPluginConan(ConanFile):
             tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
         if self.settings.os == "Linux" or self.settings.os == "Macos":
             tc.variables["CMAKE_CXX_STANDARD_REQUIRED"] = "ON"
-        tc.variables["CMAKE_PREFIX_PATH"] = qt_root
+        prefix_path = qt_root
+        if os_info.is_macos:
+            proc = subprocess.run("brew --prefix libomp",  shell=True, capture_output=True)
+            prefix_path = prefix_path + f";{proc.stdout.decode('UTF-8').strip()}"
+        tc.variables["CMAKE_PREFIX_PATH"] = prefix_path
         tc.generate()
 
     def _configure_cmake(self):
