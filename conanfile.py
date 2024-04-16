1ff31feae5fa1bc809808f5c81b221df05842848
Alexander Vieth <a.vieth@tudelft.nl>
2024-04-16 11:44:13 +0200

    Add unity build option  (#14)
    * Add unity build option
    
    * Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -116,6 +116,9 @@ class PcaPluginConan(ConanFile):
         # Give the installation directory to CMake
         tc.variables["MV_INSTALL_DIR"] = self.install_dir
         
+        # Set some build options
+        tc.variables["MV_UNITY_BUILD"] = "ON"
+        
         tc.generate()
 
     def _configure_cmake(self):
